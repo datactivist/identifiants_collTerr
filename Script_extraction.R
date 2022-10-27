ed79d8efd506f6862635a712a9b191c366911c7f
DianeThy <diane.thierry.3asd@gmail.com>
2022-10-27 11:40:38 +0200

    coquille variable test + script au porpre

--- Script_extraction.R
+++ Script_extraction.R
@@ -11,7 +11,7 @@ library(tidyverse)
 OFGL_regions <- read_delim("https://data.ofgl.fr/explore/dataset/ofgl-base-regions-consolidee/download/?format=csv&disjunctive.reg_name=true&disjunctive.agregat=true&refine.agregat=D%C3%A9penses+totales&refine.exer=2020&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B", ";")
 
 # On garde les champs qui nous intéressent 
-OFGL_regions <- OFGL_regions[,c(4:7)]
+OFGL_regions <- OFGL_regions %>% select(`Code Insee 2021 Région`:`Code Siren Collectivité`)
 
 # On les renomme
 OFGL_regions <- OFGL_regions %>% rename(COG = `Code Insee 2021 Région`,
@@ -20,11 +20,11 @@ OFGL_regions <- OFGL_regions %>% rename(COG = `Code Insee 2021 Région`,
                                         SIREN = `Code Siren Collectivité`)
     
 # On réordonne 
-OFGL_regions <- OFGL_regions[,c(2,4,1,3)]
+OFGL_regions <- OFGL_regions %>% select(nom, SIREN, COG, type)
 
 # On met au bon format les variables 
-OFGL_regions$SIREN <- as.numeric(OFGL_regions$SIREN)
-OFGL_regions$COG <- as.character(OFGL_regions$COG)
+OFGL_regions <- OFGL_regions %>% mutate(SIREN = as.character(SIREN),
+                                        COG = as.character(COG))
 
 # On trie les observations avec COG par ordre croissant
 OFGL_regions <- OFGL_regions %>% arrange(COG)
@@ -40,7 +40,7 @@ OFGL_regions <- OFGL_regions %>% arrange(COG)
 OFGL_departements <- read_delim("https://data.ofgl.fr/explore/dataset/ofgl-base-departements-consolidee/download/?format=csv&disjunctive.reg_name=true&disjunctive.dep_tranche_population=true&disjunctive.dep_name=true&disjunctive.agregat=true&refine.exer=2020&refine.agregat=D%C3%A9penses+totales&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B", ";")
 
 # On garde les champs qui nous intéressent 
-OFGL_departements <- OFGL_departements[,c(3,7:10)]
+OFGL_departements <- OFGL_departements %>% select(`Code Insee 2021 Région`, `Code Insee 2021 Département`:`Code Siren Collectivité`)
 
 # On les renomme
 OFGL_departements <- OFGL_departements %>% rename(COG = `Code Insee 2021 Département`,
@@ -50,27 +50,25 @@ OFGL_departements <- OFGL_departements %>% rename(COG = `Code Insee 2021 Départ
                                                   SIREN = `Code Siren Collectivité`)
 
 # On réordonne 
-OFGL_departements <- OFGL_departements[,c(3,5,2,4,1)]
+OFGL_departements <- OFGL_departements %>% select(nom, SIREN, COG, type, code_region)
 
-# On modifie le type "DEPT" par "DEP"
-OFGL_departements$type <- "DEP"
+# Modification des variables 
+OFGL_departements <- OFGL_departements %>% mutate(type = "DEP", # on modifie le type "DEPT" par "DEP"
+                                                  SIREN = as.character(SIREN), # variables au bon format
+                                                  code_region = as.character(sprintf("%02d", code_region)), # ajout d'un "0" pour les COG de 1 à 9
+                                                  COG_3digits = str_pad(COG, 3, pad = "0")) %>% # COG avec 3 digits pour permettre plus de jointures
+    select(nom:type,COG_3digits, code_region)
 
-# On met au bon format les variables 
-OFGL_departements$SIREN <- as.numeric(OFGL_departements$SIREN)
+# Ajout de la Corse (pas présente dans les données) + Haut et Bas Rhin (nom et COG mal renseignés)
+OFGL_departements <- OFGL_departements %>% add_row(nom = c("Corse-du-Sud","Haute-Corse", "Bas-Rhin", "Haut-Rhin"), 
+                                                   SIREN = c("222000028","222000036", "226700011", "226800019"), 
+                                                   COG = c("2A","2B", "67", "68"), 
+                                                   type = c("DEP","DEP", "DEP","DEP"), 
+                                                   code_region = c("94","94", "44", "44"), 
+                                                   COG_3digits = c("02A","02B", "067", "068"))
 
-# Pour le code de la région on ajoute un "0" pour les COG de 1 à 9
-OFGL_departements$code_region <- sprintf("%02d", OFGL_departements$code_region)
-
-# On trie les observations avec COG par ordre croissant
-OFGL_departements <- OFGL_departements %>% arrange(COG) 
-
-# On créé une autre colonne du COG du département en mettant cette fois 3 digits (pour permettre les jointures si le COG est entré avec 2 ou 3 digits)
-OFGL_departements$COG_3digits <- OFGL_departements$COG
-OFGL_departements[c(1:95),]$COG_3digits <- paste0("0", OFGL_departements[c(1:95),]$COG_3digits)
-
-# On ajoute la Corse qui n'est pas présente dans les données
-OFGL_departements <- OFGL_departements %>% add_row(nom = c("Corse-du-Sud","Haute-Corse"), SIREN = c(222000028,222000036), COG = c("2A","2B"), type = c("DEP","DEP"), code_region = c("94","94"), COG_3digits = c("02A","02B")) %>% arrange(COG) 
-
+# Modification du département "Alsace" mal renseigné et suppression "Métropole de Lyon" présente dans les EPCI
+OFGL_departements <- OFGL_departements %>% filter(COG != "67A", COG != "691") %>% arrange(COG)
 
 
 
@@ -82,7 +80,7 @@ OFGL_departements <- OFGL_departements %>% add_row(nom = c("Corse-du-Sud","Haute
 OFGL_communes <- read_delim("https://data.ofgl.fr/explore/dataset/ofgl-base-communes-consolidee/download/?format=csv&disjunctive.reg_name=true&disjunctive.dep_name=true&disjunctive.epci_name=true&disjunctive.tranche_population=true&disjunctive.tranche_revenu_imposable_par_habitant=true&disjunctive.com_name=true&disjunctive.agregat=true&refine.exer=2020&refine.agregat=D%C3%A9penses+totales&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B", ";")
 
 # On garde les champs qui nous intéressent 
-OFGL_communes <- OFGL_communes[,c(3,5,15:18)]
+OFGL_communes <- OFGL_communes %>% select(`Code Insee 2021 Région`, `Code Insee 2021 Département`, `Code Insee 2021 Commune`:`Code Siren Collectivité`)
 
 # On les renomme
 OFGL_communes <- OFGL_communes %>% rename(COG = `Code Insee 2021 Commune`,
@@ -93,25 +91,13 @@ OFGL_communes <- OFGL_communes %>% rename(COG = `Code Insee 2021 Commune`,
                                           SIREN = `Code Siren Collectivité`)
 
 # On réordonne 
-OFGL_communes <- OFGL_communes[,c(4,6,3,5,1,2)]
+OFGL_communes <- OFGL_communes %>% select(nom, SIREN, COG, type, code_departement, code_region)
 
-# On modifie le type "Commune" par "COM"
-OFGL_communes$type <- "COM"
-
-# On passe le numéro SIREN au format numérique
-OFGL_communes$SIREN <- as.numeric(OFGL_communes$SIREN)
-
-# On trie les observations avec COG par ordre croissant
-OFGL_communes <- OFGL_communes %>% arrange(COG)
-
-# On supprime le doublon
-n_distinct(OFGL_communes)  #Les Trois Lacs en 2 fois
-OFGL_communes <- OFGL_communes %>% unique()
-
-# On créé aussi la colonne du COG de département avec 3 digits
-OFGL_communes$code_departement_3digits <- OFGL_communes$code_departement
-OFGL_communes[c(1:34808),]$code_departement_3digits <- paste0("0", OFGL_communes[c(1:34808),]$code_departement_3digits)
-
+# Modification des variables 
+OFGL_communes <- OFGL_communes %>% mutate(type = "COM", # on modifie le type "Commune" par "COM"
+                                          SIREN = as.character(SIREN), # variable au bon format
+                                          code_departement_3digits = str_pad(code_departement, 3, pad = "0")) %>% # COG avec 3 digits pour permettre plus de jointures
+    select(nom:code_departement, code_departement_3digits, code_region) %>% arrange(COG)
 
 
 
@@ -121,8 +107,8 @@ OFGL_communes[c(1:34808),]$code_departement_3digits <- paste0("0", OFGL_communes
 
 # On regroupe ces 3 niveaux de collectivités pour avoir toutes les informations en une seule base
 infos_coll <- rbind(OFGL_regions, 
-                    OFGL_departements[,c(1:4)], 
-                    OFGL_communes[,c(1:4)])
+                    OFGL_departements %>% select(nom:type), 
+                    OFGL_communes %>% select(nom:type))
 
 
 
@@ -134,7 +120,7 @@ infos_coll <- rbind(OFGL_regions,
 OFGL_interco <- read_delim("https://data.ofgl.fr/explore/dataset/ofgl-base-gfp-consolidee/download/?format=csv&disjunctive.dep_name=true&disjunctive.gfp_tranche_population=true&disjunctive.nat_juridique=true&disjunctive.mode_financement=true&disjunctive.gfp_tranche_revenu_imposable_par_habitant=true&disjunctive.epci_name=true&disjunctive.agregat=true&refine.exer=2020&refine.agregat=D%C3%A9penses+totales&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B", ";")
 
 # On garde les champs qui nous intéressent 
-OFGL_interco <- OFGL_interco[,c(3,5,8,12,13)]
+OFGL_interco <- OFGL_interco %>% select(`Code Insee 2021 Région`, `Code Insee 2021 Département`, `Nature juridique 2021 abrégée`, `Code Siren 2021 EPCI`, `Nom 2021 EPCI`)
 
 # On les renomme
 OFGL_interco <- OFGL_interco %>% rename(nom = `Nom 2021 EPCI`,
@@ -144,20 +130,11 @@ OFGL_interco <- OFGL_interco %>% rename(nom = `Nom 2021 EPCI`,
                                         SIREN = `Code Siren 2021 EPCI`)
 
 # On réordonne 
-OFGL_interco <- OFGL_interco[,c(5,4,3,1,2)]
+OFGL_interco <- OFGL_interco %>% select(nom, SIREN, type, code_departement, code_region)
 
-# On modifie le type "M" par "MET"
-OFGL_interco$type <- str_replace_all(OFGL_interco$type, c("MET69" = "M",  #cas spécifique pour la métropole de Lyon
-                                                          "M" = "MET"))
-
-# On passe le numéro SIREN au format numérique
-OFGL_interco$SIREN <- as.numeric(OFGL_interco$SIREN)
-
-# On trie les observations avec COG par ordre croissant
-OFGL_interco <- OFGL_interco %>% arrange(nom)
-
-# On créé aussi la colonne du COG de département avec 3 digits
-OFGL_communes <- OFGL_communes %>% mutate(test = as.character(sprintf("%03d", as.integer(code_departement))))
+# Modification des variables 
+OFGL_interco <- OFGL_interco %>% mutate(type = str_replace_all(type, c("MET69" = "M", "MET75" = "M", "M" = "MET")),
+                                        SIREN = as.character(SIREN)) %>% arrange(nom)
 
 
 
